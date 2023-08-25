0df0f681a8b9d10c4b4971eb521d9a4a9ee38160
Odewole Saheed Adesina <odewolesaheed@gmail.com>
2023-08-25 11:05:22 +0100

    Deploy App

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="" exact element={<Home />} />
           <Route path="/" exact element={<Home />} />
+          <Route path="" exact element={<Home />} />
           <Route path="/services" exact element={<Services />} />
           <Route path="/products" exact element={<Products />} />
           <Route path="/sign-up" exact element={<SignUp />} />
